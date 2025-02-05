552834707fa71e784b4f77137f9b53a01cb9b6ec
Stefan Kruik <s.kruik@icloud.com>
2025-02-05 22:38:25 +0100

    CI/CD Fix

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 export HOME=/home/svkruik
-export PATH=/root/.nvm/versions/node/v20.15.1/bin:$PATH
 
 # Git
 cd ..
-git config --global --add safe.directory /home/svkruik/Documents/GitHub/Lumen
+git config --global --add safe.directory "$HOME/Documents/GitHub/Lumen"
 git reset --hard
 git pull
 echo "Git setup complete."
